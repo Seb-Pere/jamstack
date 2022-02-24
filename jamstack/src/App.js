07ab439a26287b6ba76c20a3fe384a1b76602271
Seb-Pere <70896756+Seb-Pere@users.noreply.github.com>
2022-02-24 08:44:04 +0100

    first

---
+++ jamstack/src/App.js
@@ -0,0 +1,18 @@
+import Header from "./components/Header";
+import Services from "./components/Services";
+import Info from "./components/Info";
+import Contact from "./components/Contact";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Services />
+      <Info />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
